25d1a4cb1dbc26abc4c9940a73b8bccacb64f00d
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-09-03 21:08:33 +0500

    color

--- app.js
+++ app.js
@@ -25,55 +25,75 @@
 
 const display = document.getElementById("color")
 const colors = ["Green","Red","Blue","purple"];
-display.innerHTML+=`<li><button>${colors.slice(0,1)}</button></li>`
-display.innerHTML+=`<li><button>${colors.slice(1,2)}</button></li>`
-display.innerHTML+=`<li><button>${colors.slice(2,3)}</button></li>`
-display.innerHTML+=`<li><button>${colors.slice(3,4)}</button></li>`
+const back = document.querySelector('body')
+// display.innerHTML+=`<li><button>${colors.slice(0,1)}</button></li>`
+// display.innerHTML+=`<li><button>${colors.slice(1,2)}</button></li>`
+// display.innerHTML+=`<li><button>${colors.slice(2,3)}</button></li>`
+// display.innerHTML+=`<li><button>${colors.slice(3,4)}</button></li>`
 
 
-// function createColorButtons() {
-//       const display = document.getElementById("color");
-//       const colors = ["Green", "Red", "Blue", "Purple"];
+
+
+function addColor(g) {
+      switch (back) {
+            case back.style.backgroundColor == "white":
     
-//       colors.forEach(color => {
-//         const li = document.createElement("li");
-//         const button = document.createElement("button");
-//         button.textContent = color;
-//         li.appendChild(button);
-//         display.appendChild(li);
-//       });
-// }
-    
-// // Call the function to create the color buttons
-// display.innerHTML.style.backgroundColour ="Green"
-// createColorButtons();
-    
-
-// function green() {
-//       display.innerHTML.style.backgroundColour ="green"
-// }
-
-
-
-
-
-
-
-
-
-
-function addColor(green) {
-      // Assuming you have a display element to show the selected color
-      const display = document.getElementById("container");
-    
-      // Clear previous content and set the new color
-      display.innerHTML = green;
-      display.innerHTML.style.backgroundColour="Green"
+                break;
+            case back.style.backgroundColor = "green":
+                break;
+      }
 }
+ 
+function addColorse(o) {
+      switch (back) {
+            case back.style.backgroundColor == "white":
     
+                break;
+            case back.style.backgroundColor = "orangered":
+                break;
+      }
+}   
+
+function addColorr(b) {
+      switch (back) {
+            case back.style.backgroundColor == "orange":
+    
+                break;
+            case back.style.backgroundColor = "blue":     
+                break;
+
+            case back.style.color = "black":     
+            break;
+                
+        }
+}
+
+function addColorrr(p) {
+      switch (back) {
+            case back.style.backgroundColor == "blue":
+    
+                break;
+            case back.style.backgroundColor = "palevioletred":
+                break;
+        }
+}
+
+function addColorss(pp) {
+      switch (back) {
+            case back.style.backgroundColor == "pink":
+    
+                break;
+            case back.style.backgroundColor = "purple":
+                break;
+        }
+}
 
 
-
+const item =document.querySelector("#blue")
+if (item) {
+      item.innerHTML.style.Color ="black";
+      
+}
 
 
 
